2147d3599533b080b1dcfae84b8bc0b031591f48
makaleite <makaleite@rd.com.br>
2022-09-12 16:43:42 -0300

    finished

---
+++ backend/src/resolvers/UserResolver.ts
@@ -0,0 +1,30 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../models/User";
+import crypto from 'crypto';
+
+// query - Buscar dados;
+// mutation - Criar, Alterar ou Deletar dados;
+
+@Resolver()
+export class UserResolver {
+    private data: User[] = [];
+
+    @Query(() => [User])
+    async users() {
+        return this.data;
+    }
+
+    @Mutation(() => User)
+    async createUser(
+        @Arg('name') name: string
+    ) {
+        const user = {
+            id: crypto.randomUUID(),
+            name,
+        };
+
+        this.data.push(user);
+
+        return user;
+    }
+}
